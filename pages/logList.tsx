6258b999f0b398aab8b647071bc703c96b7401cf
Tolga Şükrü Özkan <ozkanozkan3233@gmail.com>
2024-05-21 16:51:31 +0300

    LogList page errors solved.

--- pages/logList.tsx
+++ pages/logList.tsx
@@ -1,65 +1,57 @@
 import React from 'react';
-import {
-  MaterialReactTable,
-} from 'material-react-table';
-import { LogsServiceClient } from '../src/app/generated/logs_grpc_web_pb'; // Oluşturulan gRPC istemcisi
-import { LogRequest } from '../src/app/generated/logs_pb'; // Oluşturulan gRPC mesajları
+import { GetServerSideProps } from 'next';
+import { MaterialReactTable } from 'material-react-table'; // Correct import
+import { LogsMessages_Log, LogsMessages_logLevel } from '../src/compiled/logsCollector'; // Ensure this import is correct
+import { LogsRequest, LogsResponse, LogsServiceClient } from '../src/compiled/logs'; // Ensure this import is correct
+import { ChannelCredentials } from '@grpc/grpc-js';
 
-const HomePage = ({ logs }) => {
+interface Log {
+  id: string;
+  title: string;
+  project: string;
+  level: string;
+  date: string;
+}
+
+interface LogListProps {
+  logs: Log[];
+}
+
+const LogList: React.FC<LogListProps> = ({ logs }) => {
   const columns = [
-    {
-      accessorKey: 'id', //access nested data with dot notation
-      header: 'Id',
-      size: 100,
-    },
-    {
-      accessorKey: 'title',
-      header: 'Title',
-      size: 150,
-    },
-    {
-      accessorKey: 'project', //normal accessorKey
-      header: 'Project',
-      size: 200,
-    },
-    {
-      accessorKey: 'level',
-      header: 'Level',
-      size: 150,
-    },
-    {
-      accessorKey: 'date',
-      header: 'Date',
-      size: 150,
-    },
+    { accessorKey: 'id', header: 'Id', size: 100 },
+    { accessorKey: 'title', header: 'Title', size: 150 },
+    { accessorKey: 'project', header: 'Project', size: 200 },
+    { accessorKey: 'level', header: 'Level', size: 150 },
+    { accessorKey: 'date', header: 'Date', size: 150 },
   ];
 
   return (
     <MaterialReactTable
-      columns={columns}
-      data={logs}
-      enableFullScreenToggle={false}
-      initialState={{ isFullScreen: true }}
-    />
+    columns={columns}
+    data={logs}
+    enableFullScreenToggle={false}
+    initialState={{ isFullScreen: true }}    
+    ></MaterialReactTable>
   );
 };
 
-export const getServerSideProps = async (context) => {
-  const client = new LogsServiceClient('http://localhost:8888'); // gRPC-Web proxy adresi
-  const request = new LogRequest();
-    // Gerekli parametreleri ayarlayın
-  request.setPage(1);
-  request.setPageSize(10);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const client = new LogsServiceClient('http://localhost:8888', ChannelCredentials.createInsecure());
+  const request = LogsRequest.create({ page: 1, pageSize: 10, index: "", filters: [] });
 
   return new Promise((resolve, reject) => {
-    client.list(request, {}, (err, response) => {
+    client.list(request, (err: any, response: LogsResponse) => {
       if (err) {
         console.error('Error fetching data:', err);
         reject(err);
       } else {
-        const logs = response.getLogsList().map(log => ({
-          ...log.toObject(),
-          date: new Date(log.getDate()).toLocaleString() // Tarih formatlama
+        const logs = response.logs.map((log: LogsMessages_Log) => ({
+          id: log.id,
+          title: log.message,
+          project: log.application?.name || '',
+          level: LogsMessages_logLevel[log.level],
+          date: new Date(log.logTime).toLocaleString(),
         }));
         resolve({ props: { logs } });
       }
@@ -67,4 +59,4 @@ export const getServerSideProps = async (context) => {
   });
 };
 
-export default HomePage;
+export default LogList;
